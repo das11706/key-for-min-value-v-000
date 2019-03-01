6aaf15e5d52fb647930289ef6bc3ce2a8e460050
Dan Suarez <suarez.daniel101@gmail.com>
2019-03-01 02:03:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     if value < smalley
       smallname = name
-    elsif value == {}
+    elsif name_hash = {}
       smallname = nil
 
     end
